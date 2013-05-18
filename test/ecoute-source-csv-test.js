4c03e8798b77ccec6f233b3f29155a795df6b709
Martin Gausby <martin@gausby.dk>
2013-05-18 13:17:39 +0200

    Implemented basic functionality using the csv and glob modules. Bumped to version 0.0.1

--- test/ecoute-source-csv-test.js
+++ test/ecoute-source-csv-test.js
@@ -12,8 +12,69 @@ var refute = buster.assertions.refute;
 
 var basicConfig = {};
 
-buster.testCase('An Ecoute CSV source', {
-    'should state the obvious': function () {
-        assert.isTrue(true);
+buster.testCase('A CSV source', {
+    'should implement a refresh function': function () {
+        var test = new Source();
+
+        assert.isFunction(test.refresh);
+    },
+    'should implement an initialize function': function () {
+        var test = new Source();
+
+        assert.isFunction(test.initialize);
+    }
+});
+
+buster.testCase('refresh', {
+    '// should call its callback method at some point': function () {
+        var test = new Source(),
+            callback = this.spy()
+        ;
+
+        test.refresh(callback);
+        assert.calledOnce(callback);
+    },
+    'should parse a csv file and return its output': function (done) {
+        var test = new Source();
+        test.files = ['test/assets/foo.csv'];
+
+        test.refresh(function(err, data) {
+            assert.equals(data, [{foo: 'bar'}]);
+            done();
+        });
+    }
+});
+
+buster.testCase('initialization', {
+    'should call its callback method at some point': function (done) {
+        var test = new Source({});
+
+        test.initialize(function(err) {
+            assert.defined(err);
+            done();
+        });
+    },
+    'should check if the source file exist': function (done) {
+        var test = new Source({
+            'file': 'test/assets/foo.csv'
+        });
+        test.initialize(function(err) {
+            refute.exception(function() {
+                throw err;
+            });
+            done();
+        });
+    },
+    'should return an error if the source file does not exist': function (done) {
+        var test = new Source({
+            'file': 'test/assets/non-existent.csv'
+        });
+
+        test.initialize(function(err) {
+            assert.exception(function() {
+                throw err;
+            });
+            done();
+        });
     }
 });
